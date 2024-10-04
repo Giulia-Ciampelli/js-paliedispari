cf405db4d71871e718e691fa2d0660f78270ddf6
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-04 16:29:00 +0200

    refactor: delete unnecessary comments and logs

--- js-palindromi/assets/main.js
+++ js-palindromi/assets/main.js
@@ -1,15 +1,5 @@
-console.log('im alive');
-
 // Palidroma
 // Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
-
-// ricerca di metodi utili:
-// .split();
-// .reverse(), più compatibile, in uso dal 1997;
-// .toReverse(), torna una nuova stringa;
-// .length;
-// .every(), forse inutile fuori da un array;
-// .join(), forse inutile fuori da un array;
 
 // funzione in cui si verifica una stringa con la metà al contrario di sè stessa
 function seiPalindroma(word) {
