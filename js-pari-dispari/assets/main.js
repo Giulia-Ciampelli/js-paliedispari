33d55617b1ee80635cbf2b4eca42d54a7df01f0c
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-04 16:28:03 +0200

    refactor: delete unnecessary comments and logs

--- js-pari-dispari/assets/main.js
+++ js-pari-dispari/assets/main.js
@@ -1,21 +1,11 @@
-console.log('im alive too');
-
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
-
-// metodi da usare:
-// Math.floor(Math.random());
-// somma aritmetica +;
-// resto %;
-// prompt;
 
 // creazione della funzione che verifica se il risultato è pari o dispari
 function pari_dispari(user) {
     user = Number(prompt('Inserisci un numero da 1 a 5'));
     pc = Math.floor(Math.random() * 5) + 1;
     sum = user + pc;
-    console.log(`numero generato: ${pc}`);
-    console.log(sum);
     
     if ((sum % 2 == 0 && evenOdd === 'pari') || (sum % 2 !== 0 && evenOdd === 'dispari')) {
         alert('Hai vinto!')
@@ -27,7 +17,6 @@ function pari_dispari(user) {
 
 // variazione per permettere di accedere alla funzione pari_dispari
 let evenOdd = prompt('pari o dispari?');
-console.log(evenOdd);
 
 // if statement per assicurarsi che i prompt giusti facciano partire la funzione
 if (evenOdd === 'pari' || evenOdd === 'dispari'){
