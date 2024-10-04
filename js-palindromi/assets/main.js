5356b172e0930859e0404d539a31d7435d060f58
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-04 20:27:08 +0200

    fix: tutor tip,  boolean check variable and alerts out of the function

--- js-palindromi/assets/main.js
+++ js-palindromi/assets/main.js
@@ -1,13 +1,21 @@
 // Palidroma
 // Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 
+// variabile di verifica booleana
+let check = false;
+
 // funzione in cui si verifica una stringa con la metà al contrario di sè stessa
 function seiPalindroma(word) {
     if (word.split('').reverse().join('') === word) {
-        alert('La parola è palindroma');
-    }
-    else {
-        alert('La parola non è palindroma');
+        check = true;
     }
 }
 seiPalindroma(word = prompt('Inserisci una parola'));
+
+// if statement per gli alert (messi fuori dalla funzione per far sì che essa possa essere invocata più volte senza ulteriori modifiche)
+if (check) {
+    alert('La parola è palindroma');
+}
+else {
+    alert('La parola non è palindroma');
+}
