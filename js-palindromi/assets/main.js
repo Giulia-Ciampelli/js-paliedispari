5878f16d3c3839125fd5806d2179c02ce0716182
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-04 15:18:32 +0200

    docs: first try with split, reverse, join methods

--- js-palindromi/assets/main.js
+++ js-palindromi/assets/main.js
@@ -4,4 +4,21 @@ console.log('im alive');
 // Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 
 // ricerca di metodi utili:
-// .split(); .reverse(); .length;
+// .split();
+// .reverse(), più compatibile, in uso dal 1997;
+// .toReverse(), torna una nuova stringa;
+// .length;
+// .every(), forse inutile fuori da un array;
+// .join(), forse inutile fuori da un array;
+
+// funzione in cui si verifica una stringa con la metà al contrario di sè stessa
+function seiPalindroma(word) {
+    if (word.split().reverse().join() === word) {
+        console.log('La parola è palindroma');
+    }
+    else {
+        console.log('La parola non è palindroma');
+    }
+}
+
+console.log(seiPalindroma('anna'));
