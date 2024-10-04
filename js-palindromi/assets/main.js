3478510ff1636517d35a5a30fc1d557b658ace8f
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-04 14:58:16 +0200

    chore: methods research

--- js-palindromi/assets/main.js
+++ js-palindromi/assets/main.js
@@ -2,3 +2,6 @@ console.log('im alive');
 
 // Palidroma
 // Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+// ricerca di metodi utili:
+// .split(); .reverse(); .length;
