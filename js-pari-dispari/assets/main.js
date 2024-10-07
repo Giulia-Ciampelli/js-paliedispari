5796fef58ed8845f77f998d487c4fa6bfdb545cf
Giulia-Ciampelli <g.ciampelli2505@gmail.com>
2024-10-07 09:45:19 +0200

    fix: apply tutor tip, alerts and prompt outside the function

--- js-pari-dispari/assets/main.js
+++ js-pari-dispari/assets/main.js
@@ -1,17 +1,16 @@
 // Pari e Dispari
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto.
 
+// creazione di variabile di verifica booleana
+let check = false;
+
 // creazione della funzione che verifica se il risultato è pari o dispari
 function pari_dispari(user) {
-    user = Number(prompt('Inserisci un numero da 1 a 5'));
     pc = Math.floor(Math.random() * 5) + 1;
     sum = user + pc;
     
     if ((sum % 2 == 0 && evenOdd === 'pari') || (sum % 2 !== 0 && evenOdd === 'dispari')) {
-        alert('Hai vinto!')
-    }
-    else {
-        alert('Hai perso!');
+        check = true;
     }
 }
 
@@ -19,9 +18,17 @@ function pari_dispari(user) {
 let evenOdd = prompt('pari o dispari?');
 
 // if statement per assicurarsi che i prompt giusti facciano partire la funzione
-if (evenOdd === 'pari' || evenOdd === 'dispari'){
-    pari_dispari();
+if (evenOdd === 'pari' || evenOdd === 'dispari'){    
+    pari_dispari(Number(prompt('Inserisci un numero da 1 a 5')));
 }
 else {
     alert('Errore: assicurati di scrivere bene "pari" o "dispari"!');
+}
+
+// if statement per gli alert
+if (check) {
+    alert('Hai vinto!');
+}
+else {
+    alert('Hai perso!');
 }
